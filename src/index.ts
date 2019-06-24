b0c5693881428340f1b60f322d8c3ee72bf94215
alexgpeppe <alexgpeppe84@hotmail.it>
2019-06-24 15:48:22 +0200

    [#166791583] Configure environment with docker

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-// TODO: write you code here
+import app from "./app";
+import { log } from "./utils/logger";
 
-process.stdout.write("Hello world");
+const server = app.listen(app.get("port"), () => {
+  log.info(
+    "  App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
+  log.info("  Press CTRL-C to stop\n");
+});
+
+export default server;
