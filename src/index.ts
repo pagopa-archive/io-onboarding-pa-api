b61728004b7a02a548c8c2b389c6d01c72d0b637
alexgpeppe <alexgpeppe84@hotmail.it>
2019-06-26 09:00:24 +0200

    [#166791583] Remove unneeded variable in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import newApp from "./app";
 import { log } from "./utils/logger";
 
-// tslint:disable-next-line:no-let
-let server;
 newApp()
   .then(app => {
-    server = app.listen(app.get("port"), () => {
+    app.listen(app.get("port"), () => {
       log.info(
         "  App is running at http://localhost:%d in %s mode",
         app.get("port"),
