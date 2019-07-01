6198318427b6e8b96ffcc8469b7e6105452405ab
alexgpeppe <alexgpeppe84@hotmail.it>
2019-07-01 12:19:19 +0200

    [#166728960] Add endpoint to search public administrations from IPA

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+import axios from "axios";
 import * as bodyParser from "body-parser";
 import * as express from "express";
-import { Express } from "express";
+import { Express, Request, Response } from "express";
+import { query, validationResult } from "express-validator";
 import { Pool } from "pg";
 
 import { log } from "./utils/logger";
+
+import {
+  IIpaSearchResponseBody,
+  IIpaSearchResult
+} from "./types/PublicAdministration";
 
 const postgres = new Pool();
 
@@ -28,9 +35,7 @@ export default async function newApp(): Promise<Express> {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.get("/", (_0, res) => {
-    res.json({ text: "Hello world!" });
-  });
+  registerRoutes(app);
 
   try {
     await createConnection(postgres);
@@ -39,4 +44,77 @@ export default async function newApp(): Promise<Express> {
     process.exit(1);
   }
   return app;
+}
+
+function registerRoutes(app: Express): void {
+  app.get(
+    "/public-administrations",
+    [
+      query("search")
+        .not()
+        .isEmpty()
+        .withMessage("a value is required")
+        .isLength({ min: 3 })
+        .withMessage("value must have at least 3 characters")
+    ],
+    async (req: Request, res: Response) => {
+      const validationErrors = validationResult(req);
+      if (!validationErrors.isEmpty()) {
+        return res.status(400).json(validationErrors.array());
+      }
+      const searchString = req.query.search;
+      const requestUrl =
+        "https://elasticsearch.developers.italia.it/indicepa/_search";
+      const requestBody = {
+        query: {
+          bool: {
+            should: [
+              {
+                nested: {
+                  path: "office",
+                  query: {
+                    multi_match: {
+                      fields: ["office.code", "office.description"],
+                      operator: "and",
+                      query: searchString
+                    }
+                  }
+                }
+              },
+              {
+                multi_match: {
+                  fields: ["ipa", "description"],
+                  operator: "and",
+                  query: searchString
+                }
+              }
+            ]
+          }
+        }
+      };
+      try {
+        const searchResponse = await axios.post<IIpaSearchResponseBody>(
+          requestUrl,
+          requestBody
+        );
+        const publicAdministrations = searchResponse.data.hits.hits
+          .map(hit => hit._source)
+          .reduce(
+            (previous: ReadonlyArray<IIpaSearchResult>, current) => [
+              ...previous,
+              {
+                description: current.description,
+                ipa: current.ipa,
+                pec: current.pec
+              }
+            ],
+            []
+          );
+        return res.json(publicAdministrations);
+      } catch (error) {
+        log.error(error);
+        return res.status(500).end(error);
+      }
+    }
+  );
 }
