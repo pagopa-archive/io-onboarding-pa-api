d0807757b1771be6c7876cef5bb0a06663db00fb
alexgpeppe <alexgpeppe84@hotmail.it>
2019-09-03 15:56:29 +0200

    [#168215511] Delete public administrations not existing on IPA anymore

--- src/services/ipaPublicAdministrationService.ts
+++ src/services/ipaPublicAdministrationService.ts
@@ -10,40 +10,68 @@ import { IIpaPublicAdministration } from "../types/PublicAdministration";
 import { log } from "../utils/logger";
 
 export function upsertFromIpa(): Promise<void> {
-  return new Promise(async resolve => {
+  return new Promise(async (resolve, reject) => {
     const csvStream = createStream({
       delimiter: "\t"
     });
-    const indicepaResponse = await fetch(INDICEPA_URL);
-    indicepaResponse.body
-      .pipe(csvStream)
-      .pipe(
-        es.map((entry: IIpaPublicAdministration, cb: () => void) => {
-          if (entry.cf_validato !== "S") {
-            // filter out entries without a validated CF
-            return cb();
-          }
-          if (!entry.Cf || !entry.Cf.match(/^\d{2,}$/)) {
-            // filter out entries with bogus CF
-            return cb();
-          }
-          IpaPublicAdministrationModel.upsert(entry)
-            .then(() => {
-              cb();
-            })
-            .catch(error => {
-              log.error(
-                "IpaPublicAdministration upsert failed for %s: %s",
-                entry.des_amm,
-                error
-              );
-              cb();
-            });
+    try {
+      const [indicepaResponse, notUpdatedEntries] = await Promise.all([
+        fetch(INDICEPA_URL),
+        IpaPublicAdministrationModel.findAll({
+          attributes: ["cod_amm", "des_amm"]
         })
-      )
-      .on("end", () => {
-        resolve();
-      });
+      ]);
+      indicepaResponse.body
+        .pipe(csvStream)
+        .pipe(
+          es.map((entry: IIpaPublicAdministration, cb: () => void) => {
+            if (entry.cf_validato !== "S") {
+              // filter out entries without a validated CF
+              return cb();
+            }
+            if (!entry.Cf || !entry.Cf.match(/^\d{2,}$/)) {
+              // filter out entries with bogus CF
+              return cb();
+            }
+            const entryIndex = notUpdatedEntries.findIndex(
+              entryFromDb => entryFromDb.cod_amm === entry.cod_amm
+            );
+            notUpdatedEntries.splice(entryIndex, 1);
+            IpaPublicAdministrationModel.upsert(entry)
+              .then(() => {
+                cb();
+              })
+              .catch(error => {
+                log.error(
+                  "IpaPublicAdministration upsert failed for %s: %s",
+                  entry.des_amm,
+                  error
+                );
+                cb();
+              });
+          })
+        )
+        .on("end", () => {
+          // tslint:disable-next-line:no-floating-promises
+          Promise.all(
+            notUpdatedEntries.map(notUpdatedEntry =>
+              notUpdatedEntry.destroy().catch(error => {
+                log.error(
+                  "An error occurred when deleting %s IpaPublicAdministration. %s",
+                  notUpdatedEntry.des_amm,
+                  error
+                );
+              })
+            )
+          ).then(() => resolve());
+        });
+    } catch (error) {
+      log.error(
+        "An error occurred during the IpaPublicAdministration table upsert process. %s",
+        error
+      );
+      reject(error);
+    }
   });
 }
 
@@ -66,7 +94,7 @@ export async function populateIpaPublicAdministrationTable():
     }
   } catch (error) {
     log.error(
-      "An error occurred counting entries in IpaPublicAdministration table."
+      "An error occurred while populating entries in IpaPublicAdministration table."
     );
     return process.exit(1);
   }
