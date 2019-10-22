aa6a0221996e42408dbb672fdb22f863f760c55f
alexgpeppe <alexgpeppe84@hotmail.it>
2019-10-22 12:31:36 +0200

    [#169034053] Fix deletion of outdated public administrations

--- src/services/ipaPublicAdministrationService.ts
+++ src/services/ipaPublicAdministrationService.ts
@@ -12,12 +12,19 @@ import {
 } from "../types/PublicAdministration";
 import { log } from "../utils/logger";
 
+/**
+ * Upserts the public administrations from IPA and deletes the outdated ones.
+ * The public administrations info is read from a CSV file fetched from IPA web server,
+ * the content of each row is checked and upserted.
+ * At the end of the process, all the outdated entries in the db are deleted.
+ */
 export function upsertFromIpa(): Promise<void> {
   return new Promise(async (resolve, reject) => {
     const csvStream = createStream({
       delimiter: "\t"
     });
     try {
+      // notUpdatedEntries contains the references to the db entries that have not been updated,
       const [indicepaResponse, notUpdatedEntries] = await Promise.all([
         fetch(INDICEPA_URL),
         IpaPublicAdministrationModel.findAll({
@@ -28,7 +35,10 @@ export function upsertFromIpa(): Promise<void> {
         .pipe(csvStream)
         .pipe(
           es.map((parsedRow: IIpaPublicAdministrationRaw, cb: () => void) => {
+            // Check that the info from the current row is valid,
+            // if it's not, then do nothing
             IpaPublicAdministration.decode(
+              // Trim the property values of the parsed row
               Object.entries(parsedRow)
                 .map(([key, val]) => [key, val.trim()])
                 .reduce(
@@ -40,15 +50,19 @@ export function upsertFromIpa(): Promise<void> {
             ).fold(
               () => cb(),
               newEntry => {
-                const entryIndex = notUpdatedEntries.findIndex(
-                  entryFromDb => entryFromDb.cod_amm === newEntry.cod_amm
-                );
-                notUpdatedEntries.splice(entryIndex, 1);
                 IpaPublicAdministrationModel.upsert(newEntry)
                   .then(() => {
+                    // Find the reference to the currently upserted info in the notUpdatedEntries array and remove it
+                    const entryIndex = notUpdatedEntries.findIndex(
+                      entryFromDb => entryFromDb.cod_amm === newEntry.cod_amm
+                    );
+                    if (entryIndex !== -1) {
+                      notUpdatedEntries.splice(entryIndex, 1);
+                    }
                     cb();
                   })
                   .catch(error => {
+                    // Log the upsert error and do nothing
                     log.error(
                       "IpaPublicAdministration upsert failed for %s: %s",
                       newEntry.des_amm,
@@ -64,6 +78,7 @@ export function upsertFromIpa(): Promise<void> {
           // tslint:disable-next-line:no-floating-promises
           Promise.all(
             notUpdatedEntries.map(notUpdatedEntry =>
+              // Delete the outdated public administration
               notUpdatedEntry.destroy().catch(error => {
                 log.error(
                   "An error occurred when deleting %s IpaPublicAdministration. %s",
