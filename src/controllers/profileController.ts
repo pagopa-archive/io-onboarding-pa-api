9af12b06b7c820b006b96b7fe2df8cabf71c6b6c
alexgpeppe <alexgpeppe84@hotmail.it>
2019-11-07 10:42:47 +0100

    [#169591573] Add condition to send work email change notification

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -57,26 +57,28 @@ export default class ProfileController {
             workEmail
           );
           return errorResponseOrSuccessResponse.map(response => {
-            const emailText = localeIt.profileController.editProfile.notificationEmail.content.replace(
-              "%s",
-              user.givenName
-            );
-            // tslint:disable-next-line:no-floating-promises
-            this.emailService
-              .send({
-                html: emailText,
-                subject:
-                  localeIt.profileController.editProfile.notificationEmail
-                    .subject,
-                text: emailText,
-                to: workEmail
-              })
-              .catch(error =>
-                log.error(
-                  "Failed to send email notification for work email change. %s",
-                  error
-                )
+            if (response.value.work_email !== response.value.email) {
+              const emailText = localeIt.profileController.editProfile.notificationEmail.content.replace(
+                "%s",
+                user.givenName
               );
+              // tslint:disable-next-line:no-floating-promises
+              this.emailService
+                .send({
+                  html: emailText,
+                  subject:
+                    localeIt.profileController.editProfile.notificationEmail
+                      .subject,
+                  text: emailText,
+                  to: workEmail
+                })
+                .catch(error =>
+                  log.error(
+                    "Failed to send email notification for work email change. %s",
+                    error
+                  )
+                );
+            }
             return response;
           }).value;
         }
